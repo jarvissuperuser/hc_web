87957798e937c501ce81dbcf84f158a65920df3a
Timothy Mugadza <mugadzatt001@gmail.com>
2018-02-19 19:30:04 +0200

    jira project

--- g.php
+++ g.php
@@ -2,13 +2,24 @@
 
 $c = curl_init();
 
-$url = "https://tmgdigital.atlassian.net/rest/api/2/search?RapidViewId=2&view=planning.nodetail&jql=status!=Done%20AND%20issuetype=Task";
+$url = "https://tmgdigital.atlassian.net/rest/api/2/search?RapidViewId=2&jql=status!=Done";
+try{
 
-curl_setopt($c, CURLOPT_URL,$url);
+$cmd  =  file_get_contents('info.ini');
+echo shell_exec($cmd);
+/*curl_setopt($c, CURLOPT_URL,$url);
 $auth =  file_get_contents("auth.ini");
+
 $post = ['startAt'=>'0','maxresults'=>'200'];
-$hA = "Authorisation: BASIC $auth";
+$hA = "Authorization: BASIC $auth";
 $hA2 =  "Content-Type: application/json";
+//$hA3 =  "Content-Length: ".strlen(json_encode($post));
 curl_setopt($c,CURLOPT_HTTPHEADER,[$hA,$hA2]);
-curl_setopt($c,CURLOPT_POSTFIELDS,json_encode($post));
-curl_exec($c);
+curl_setopt($c,CURLOPT_CUSTOMREQUEST,'GET');
+//curl_setopt($c,CURLOPT_POSTFIELDS,json_encode($post));
+curl_exec($c);*/
+//echo  $hA;
+}
+catch(Exception $e){
+	echo $e->getTraceAsString();
+}

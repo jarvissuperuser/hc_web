b73a757f5e3bd90498f9925dd78cc3eb00661bb7
Timothy Mugadza <MugadzaT@timesmedia.co.za>
2018-02-22 13:45:45 +0200

    cherry view fix

--- js/button.js
+++ js/button.js
@@ -39,7 +39,7 @@ var btn = {
             i.forEach(task => {
                 if (task.fields.subtasks.length > 0) {
                     var b = "<button class='w3-btn' onclick='data.do_(" +
-                        cnt + ",\"subtasks\",\"-\",1)'>" + task.fields.summary + "</button>";
+                        cnt + ",\"subtasks\",\"-\",1)'><p>" + task.fields.summary + "</p></button>";
                     $(".cntrl .w3-card-4 .w3-content").append(b);
                 }
                 cnt = cnt + 1;

104d136ad92f8f7ccb40d85b07b38e7885d20b14
Timothy Mugadza <MugadzaT@timesmedia.co.za>
2018-02-27 15:49:46 +0200

    status filters

--- gv2.php
+++ gv2.php
@@ -3,7 +3,7 @@
 $c = curl_init();
 
 $url2 = "https://tmgdigital.atlassian.net/rest/api/2/myself";
-$url = "https://tmgdigital.atlassian.net/rest/api/2/search?RapidViewId=2&maxResults=40&jql=(issueType=Improvement%20OR%20issueType=Bug%20OR%20issueType=Task%20OR%20issueType=Story%20OR%20issueType=10006)%20AND%20status!=done";
+$url = "https://tmgdigital.atlassian.net/rest/api/2/search?RapidViewId=2&maxResults=100&jql=(issueType=Improvement%20OR%20issueType=Bug%20OR%20issueType=Task%20OR%20issueType=Story%20OR%20issueType=10006)%20AND%20(status!=Done%20AND%20status!=10100%20AND%20status!=10600)";
 try{
 
 //$cmd  =  file_get_contents('info.ini');

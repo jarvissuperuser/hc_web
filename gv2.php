33212ae11505f4a41df11b8fd03e57e57e6a2048
Timothy Mugadza <MugadzaT@timesmedia.co.za>
2018-02-27 15:34:12 +0200

    real test

--- gv2.php
+++ gv2.php
@@ -8,7 +8,7 @@ try{
 
 //$cmd  =  file_get_contents('info.ini');
 //echo shell_exec($cmd);
-curl_setopt($c, CURLOPT_URL,$url2);
+curl_setopt($c, CURLOPT_URL,$url);
 $auth =  file_get_contents("auth.ini");
 
 $post = ['startAt'=>'0','maxresults'=>'200'];
@@ -17,8 +17,8 @@ $hA2 =  "Content-Type : application/json";
 //curl_setopt($c,CURLOPT_CUSTOMREQUEST,'GET');
 //$hA3 =  "Content-Length: ".strlen(json_encode($post));
 curl_setopt($c,CURLOPT_HTTPHEADER,[$hA,$hA2]);
-//curl_setopt($c,CURLOPT_HEADER,0);
-// curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c,CURLOPT_HEADER,0);
+ curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($c,CURLOPT_POSTFIELDS,json_encode($post));
 curl_exec($c);
 //echo  $hA;
